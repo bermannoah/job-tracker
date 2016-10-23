28848f1b507d9602dc22c828e9747f1200c34bcd
Noah Berman <noah@noahberman.org>
2016-10-23 16:16:39 -0600

    jobs by interest page created

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,5 +5,11 @@ class DashboardController < ApplicationController
     @companies = Company.all
   end
   
+  def average_level_of_interest
+    averages = @companies.map do |company|
+      AVG(company.jobs.level_of_interest)
+    end
+  end
+  
   
 end
